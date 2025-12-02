bc29db52b8ee7fdb2cd736c6d2063bd9aee73d8e
erick <erick@192.168.1.74>
2025-12-02 11:26:11 -0600

    documentacion v1

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.siaralite 
-{
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.siaralite to javafx.fxml;
-    exports com.mycompany.siaralite;
-}
